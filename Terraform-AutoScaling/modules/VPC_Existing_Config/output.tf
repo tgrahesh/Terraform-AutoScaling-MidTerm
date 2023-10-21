303cc04f7bd72d57dd9eb1083c7a4196da2a213b
Grahesh <tgrahesh@gmail.com>
2023-10-21 23:49:47 +0100

    deleted vpc existing module

--- Terraform-AutoScaling/modules/VPC_Existing_Config/output.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-  value = data.aws_vpc.my_vpc.id
-}
-
-output "subnet_ids" {
-  value = [for s in data.aws_subnet.example : s.id]
-}
--- Terraform-AutoScaling/modules/VPC_Existing_Config/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vpc_id" {
-  type    = string
-  default = "vpc-01bbb7200bc80a573"  # Replace with your VPC ID
-}
